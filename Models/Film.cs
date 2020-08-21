ef3d770d13294a8bc2a288762380c8d752efe6b7
Denis N <service4tv@gmail.com>
2020-08-21 14:34:48 +0300

    Add project files.

---
+++ Models/Film.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cinema.Models
+{
+    public class Film
+    {
+        public int FilmID { get; set; }
+        public string FilmName { get; set; }
+    }
+}
