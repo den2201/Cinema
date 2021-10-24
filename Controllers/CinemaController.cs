c1c8f845770bf003e108cfa658b256ef2e7a5c58
dnikiforov <nikiforov_den@mail.ru>
2021-10-24 21:46:26 +0300

    Added footer helper

---
+++ Controllers/CinemaController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Cinema.Controllers
+{
+    public class CinemaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About(int? x)
+        {
+            return View(x);
+        }
+    }
+}
---
+++ Helpers/FooterHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Text.Encodings.Web;
+
+namespace Cinema.Helpers
+{
+    public static class FooterHelper
+    {
+        public static Microsoft.AspNetCore.Html.HtmlString Footer(this IHtmlHelper helper, params string [] items)
+        {
+            var footer = new TagBuilder("footer");
+            foreach(var item in items)
+            {
+                var p = new TagBuilder("p");
+                p.InnerHtml.Append(item);
+                footer.InnerHtml.AppendHtml(p);
+            }
+            var writer = new System.IO.StringWriter();
+            footer.WriteTo(writer, HtmlEncoder.Default);
+            return new HtmlString(writer.ToString());
+        }
+    }
+}
